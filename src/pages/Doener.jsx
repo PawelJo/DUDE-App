5ae51b37bb68598eb23b15d0f48a99816ab7bec0
PawelJo <57221666+PawelJo@users.noreply.github.com>
2024-02-21 22:55:29 +0100

    cleaning up no longer necessary code in vendor entries

--- src/pages/Doener.jsx
+++ src/pages/Doener.jsx
@@ -13,22 +13,7 @@ export default function Doener() {
 		error: null,
 	})
 
-	/* 	const [data, setData] = useState([]); */
-	/* 
-		useEffect(() => {
-			fetch('http://localhost:8080/vendors?category="D%C3%B6ner"')
-				.then(response => response.json())
-				.then(data => {
-					// Handle the data received from the server
-					console.log(data);
-					setData(data)
-	
-				})
-				.catch(error => {
-					console.error('Error fetching data:', error);
-	
-				});
-		}, []); */
+
 
 	return (
 		<RootLayout>
@@ -37,42 +22,6 @@ export default function Doener() {
 			<VendorGetter slug={slug} onDataFetched={setFetchedData} />
 			<VendorList data={fetchedData.data} loading={fetchedData.loading} />
 
-			{/* {data.length > 0 ? (<div>
-				<h1>RULES</h1>
-				<h2>{data[0].ruleText}</h2>
-				<table>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Category</th>
-							<th>Vendor name</th>
-							<th>Rating</th>
-							<th>Pros</th>
-							<th>Cons</th>
-							<th>GmapsLink</th>
-							<th>Date created</th>
-
-						</tr>
-					</thead>
-					<tbody>
-						{data.map(item => (
-							<tr key={item.id}>
-								<td>{item.id}</td>
-								<td>{item.category}</td>
-								<td>{item.name}</td>
-								<td>{item.rating}</td>
-								<td>{item.pros}</td>
-								<td>{item.cons}</td>
-								<td>{item.gmapsLink}</td>
-								<td>{item.dateCreated}</td>
-
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>) : (
-				<h2>Yeah yeah we are loading, chill the fuck out </h2>
-			)} */}
 		</RootLayout>
 	)
 }
--- src/pages/Spaeti.jsx
+++ src/pages/Spaeti.jsx
@@ -1,60 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import RootLayout from './layouts/RootLayout';
+import VendorGetter from './utility/VendorGetter';
+import VendorList from './layouts/VendorList';
 
 export default function Spaeti() {
-	const [data, setData] = useState([]);
 
-	useEffect(() => {
-		fetch('http://localhost:8080/vendors?category="Sp%C3%A4ti"')
-			.then(response => response.json())
-			.then(data => {
-				// Handle the data received from the server
-				console.log(data);
-				setData(data)
-			})
-			.catch(error => {
-				console.error('Error fetching data:', error);
 
-			});
-	}, []);
+	const slug = '"Späti"'
+	const [fetchedData, setFetchedData] = useState({
+		data: [],
+		loading: true,
+		error: null,
+	})
+
+
+
 	return (
 		<RootLayout>
-			<div>Späti</div>
+			<h1>Späti</h1>
 
+			<VendorGetter slug={slug} onDataFetched={setFetchedData} />
+			<VendorList data={fetchedData.data} loading={fetchedData.loading} />
 
-			<div>
-				<h1>Your Data</h1>
-				<table>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Category</th>
-							<th>Vendor name</th>
-							<th>Rating</th>
-							<th>Pros</th>
-							<th>Cons</th>
-							<th>GmapsLink</th>
-							<th>Date created</th>
-
-						</tr>
-					</thead>
-					<tbody>
-						{data.map(item => (
-							<tr key={item.id}>
-								<td>{item.id}</td>
-								<td>{item.category}</td>
-								<td>{item.name}</td>
-								<td>{item.rating}</td>
-								<td>{item.pros}</td>
-								<td>{item.cons}</td>
-								<td>{item.gmapsLink}</td>
-								<td>{item.dateCreated}</td>
-
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
 		</RootLayout>
 	)
 }
